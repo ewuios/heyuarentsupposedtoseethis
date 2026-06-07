afa8df2fe9feb85ad5afdc5633b249a6bc0393d1
ewuios <happyguy3327@gmail.com>
2026-06-07 18:09:27 -0500

    update bridge so i dont fucking get these errors

--- frida-il2cpp-bridge.js
+++ frida-il2cpp-bridge.js
@@ -1,4 +1,3 @@
-// patched by ChatGPT: frida-il2cpp-bridge (3) direct-obfuscated export style regenerated with supplied symbols
 "use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
